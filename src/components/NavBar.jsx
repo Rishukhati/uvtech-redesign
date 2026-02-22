8cb20cc7ee1c7021629e6efcd8a870c8520badf1
Rishabh singh <rishabhsingh.block@gmail.com>
2026-02-22 09:29:17 +0530

    setup project

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,22 @@
+
+function NavBar() {
+
+    return (
+        <nav>
+            <a href="#" className="flex ml-2">
+                <div
+                    className="w-10 h-11 bg-slate-950 
+            [clip-path:polygon(50%_0%,_100%_25%,_100%_75%,_50%_100%,_0%_75%,_0%_25%)]
+            flex items-center justify-center text-white font-bold">
+                    UV
+                </div>
+                <span className="flex p-2">
+                    UV
+                    <em>TECH</em>
+                    Multiservices
+                </span>
+            </a>
+        </nav >
+    );
+}
+export default NavBar
