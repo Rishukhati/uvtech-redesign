32e8e8a229f4b8bac7845b99e57dfe3921587c98
Rishabh singh <rishabhsingh.block@gmail.com>
2026-02-23 23:12:32 +0530

    add some section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import NavBar from "./components/NavBar"
 import Button from "./components/Button"
+import Hero from "./components/Hero"
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <NavBar />
       <Button />
+      <Hero />
     </>
   )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,20 @@
+
+
+function Hero() {
+    return (
+        <section>
+            <div>
+                <div className="mb-7 ml-3">
+                    <div className="w-2 h-2 rounded-full bg-orange-500 animate-pulse inline-block mr-2"></div>
+                    <span>Established 2018 · Lucknow, India · Pan-India Operations</span>
+                </div>
+                <h1>
+                    Powering
+                    <span>India's Digital</span>
+                    <span>Government</span>
+                </h1>
+            </div>
+        </section>
+    )
+}
+export default Hero
